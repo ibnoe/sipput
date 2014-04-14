0544642c12c26035a1f71e1bbd1adc2e87e66da7
MOCHAMMAD RIZKI ROMDONI <m_rizki_r@yacanet.com>
2014-04-14 20:43:20 +0700

    Commit #4

--- protected/pagecontroller/sa/dmaster/CJenisIzinUsaha.php
+++ protected/pagecontroller/sa/dmaster/CJenisIzinUsaha.php
@@ -22,5 +22,26 @@ class CJenisIzinUsaha extends MainPageSA {
 		$this->RepeaterS->DataSource=$r;
 		$this->RepeaterS->dataBind();      		
     }   
+    public function editRecord ($sender,$param) {		
+		$this->idProcess='edit';
+		$id=$this->getDataKeyField($sender,$this->RepeaterS);        
+		$this->hiddenid->Value=$id;        
+        $str = "SELECT InsIzin,NmIzin FROM jenisizinusaha WHERE RecNoIzin=$id";
+        $this->DB->setFieldTable(array('InsIzin','NmIzin'));
+        $r=$this->DB->getRecord($str);    
+		$result = $r[1];        			        		                
+        $this->txtEditInsIzinUsaha->Text=$result['InsIzin']; 
+        $this->txtEditNamaIzin->Text=$result['NmIzin'];        
+	}
+    public function updateData($sender,$param) {		
+        if ($this->Page->IsValid) {		
+            $id=$this->hiddenid->Value;            
+            $insizin=  strtoupper($this->txtEditInsIzinUsaha->Text);            
+            $namaizin=strtoupper($this->txtEditNamaIzin->Text);            
+            $str = "UPDATE jenisizinusaha SET InsIzin='$insizin',NmIzin='$namaizin' WHERE RecNoIzin=$id";
+            $this->DB->updateRecord($str);           
+            $this->redirect('dmaster.JenisIzinUsaha',true);
+        }
+	}
 }
 		
